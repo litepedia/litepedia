42bc599969c3d6f1d2d69dbd4d8dfe107692b896
Luke Rogerson <luke@orbs.com>
2023-05-25 16:53:54 +0100

    Add CORS middleware

--- handler/app.ts
+++ handler/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 
 import serverless from 'serverless-http';
 import { notFoundHandler, searchHandler, homeHandler } from './routes';
@@ -7,6 +8,8 @@ import path from 'path';
 import { replaceEncodedSpaces } from './middlewares';
 
 const app = express();
+
+app.use(cors());
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 app.engine('.html', require('ejs').__express);
